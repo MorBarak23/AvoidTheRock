edc54adacb8739f2158261f35a4d27f4fdb7f6e0
MorBarak <morbarak23@gmail.com>
2026-01-05 17:55:10 +0200

    Completed part 2 of the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,10 +20,12 @@ android {
 
     defaultConfig {
         applicationId = "com.mor.avoidtherock"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         val mapsKey = localProperties.getProperty("MAPS_API_KEY") ?: ""
         manifestPlaceholders["MAPS_API_KEY"] = mapsKey
@@ -32,17 +34,6 @@ android {
     namespace = "com.mor.avoidtherock"
     compileSdk {
         version = release(36)
-    }
-
-    defaultConfig {
-        applicationId = "com.mor.avoidtherock"
-        minSdk = 28
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
     }
 
     buildFeatures {
